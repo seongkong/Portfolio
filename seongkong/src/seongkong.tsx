e58e8a45f2a294bc64ea09bbb5275c34db5369b6
seongkong <bins506@gmail.com>
2025-06-30 01:34:05 +0900

    seongkong set up complete

---
+++ seongkong/src/seongkong.tsx
@@ -0,0 +1,12 @@
+// src/seongkong.ts
+import React from 'react';
+
+function Seongkong() {
+  return (
+    <div className="text-3xl font-bold text-green-500">
+      This is Seongkong Component!
+    </div>
+  );
+}
+
+export default Seongkong;
