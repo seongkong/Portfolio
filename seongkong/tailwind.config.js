7f2e15f5718710ff8c6d0ecc9f3c12ac1471b724
seongkong <bins506@gmail.com>
2025-06-29 22:39:42 +0900

    tailwindcss setting complete

---
+++ seongkong/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // src 내부만 체크
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
