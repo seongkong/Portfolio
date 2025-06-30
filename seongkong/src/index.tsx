db9134fe929f344d1132578bb8a0067bb610da17
seongkong <bins506@gmail.com>
2025-06-30 11:47:02 +0900

    index 수정

--- seongkong/src/index.tsx
+++ seongkong/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Seongkong from './seongkong';
+import Seongkong from './seongkong.tsx';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
