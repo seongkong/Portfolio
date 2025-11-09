d05e97513af1f4bb163878921e34c863ebab077a
seongkong <bins506@gmail.com>
2025-11-10 01:13:05 +0900

    error modify

--- seongkong/src/index.tsx
+++ seongkong/src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Seongkong from './seongkong';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
